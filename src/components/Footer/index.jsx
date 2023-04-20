c6f32c29dcb01cdd798af30206ce265e49f86a65
abdulrehman035551 <96192529+abdulrehman035551@users.noreply.github.com>
2023-04-20 13:32:26 +0500

    Update index.jsx

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
           </Stack>
           <Stack direction={{md:"row",xs:"row"}}>
           <EmailIcon sx={{marginRight:{md:"10px",xs:"10px"}}}/>
-          <Typography variant="p"  sx={{ fontFamily: { md: "Roboto", xs: "Roboto" }, fontSize: { md: "18px", xs: "14px" }, mb: { md: "30px", xs: "30px" } }}> nomanwaseem111@gmail.com</Typography>
+          <Typography variant="p"  sx={{ fontFamily: { md: "Roboto", xs: "Roboto" }, fontSize: { md: "18px", xs: "14px" }, mb: { md: "30px", xs: "30px" } }}> abdul@gmail.com</Typography>
           </Stack>
 
         </Stack>
@@ -67,7 +67,7 @@ const Footer = () => {
 
       </Stack>
       <Stack direction={{ md: "row", xs: "row" }} justifyContent={{ md: "space-between", xs: "space-between" }} pt={{ md: "15px", xs: "10px" }} sx={{ width: { md: "100%", xs: "100%" }, px: { md: "15%", xs: "4%" }, backgroundColor: { md: "#fff", xs: "#fff" }, height: { md: "70px", xs: "60px" } }}>
-        <Typography variant="p" sx={{ fontSize: { md: "18px", xs: "8px" }, fontFamily: { md: "Roboto", xs: "Roboto" }, pt: { md: "10px", xs: "5px" } }}>Ecommerce Website 2022 created by Noman </Typography>
+        <Typography variant="p" sx={{ fontSize: { md: "18px", xs: "8px" }, fontFamily: { md: "Roboto", xs: "Roboto" }, pt: { md: "10px", xs: "5px" } }}>Ecommerce Website 2022 created by abdul </Typography>
         <Box component="img" sx={{ width: { md: "330px", xs: "150px" }, height: { md: "40px", xs: "20px" } }} src={paymentImg} alt="" />
       </Stack>
     </>
